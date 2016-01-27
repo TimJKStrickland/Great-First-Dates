5654fe4f26d8a0a26e15d2e635812465c3ba9ed7
Tim JK Strickland <timjkstrickland@gmail.com>
2016-01-27 01:05:23 -0800

    update with jQuery and Knockout

--- app.js
+++ app.js
@@ -1,44 +1,159 @@
 var map;
-var infowindow;
-function initMap() {
-  var sanFrancisco = {lat: 37.7664823, lng: -122.4194691};
+var pins = [];
+var infoWindows = [];
+// calling the center outside the function
+var startCenter = {lat: 37.7664823, lng: -122.42};
+
+/** MODEL **/
+var locations = [
+  // here are the 8 hard-coded locations for the requirements:
+  {
+    name: "Ryoko's",
+    latlong: { lat: 37.7882006, lng:-122.4142544 },
+    yelpAPI: ""
+  },
+  {
+    name: "Chamber Eat + Drink",
+    latlong: { lat: 37.7830672, lng:-122.4203229 },
+    yelpAPI: ""
+  },
+  {
+    name: "Starbelly",
+      latlong: { lat: 37.7640966, lng:-122.4347866 },
+      yelpAPI: ""
+  },
+  {
+    name: "Devil's Acre",
+    latlong: { lat: 37.7976738, lng:-122.4083549 },
+    yelpAPI: ""
+  },
+  {
+    name: "Elephant Sushi",
+    latlong: { lat: 37.7986734, lng:-122.4209331 },
+    yelpAPI: ""
+  }
+];
+
+// Made so that locations remains after it is cleared 
+var locations2 = [];
+var popLocations2 = function(){
+  for (var i in locations){
+    locations2.push(locations[i]);
+  }
+};
+popLocations2();
+
+/**  VIEW **/
+// init's Google Maps API
+function initMap(){
   map = new google.maps.Map(document.getElementById('map'), {
-      center: sanFrancisco,
-      zoom: 13,
-      scrollwheel: false
+    center: startCenter,
+    zoom: 13
+  });
+  // putting all pins on the map and create the infowindow for each marker:
+
+  for(var i = 0; i < locations.length; i++){
+    var marker = new google.maps.Marker({
+      position: locations[i].latlong,
+      map: map,
+      title: locations[i].name
     });
 
-  infowindow = new google.maps.InfoWindow();
+    // adding the Infowindow to populate and creating the error message if the
+    // net breaks. contentString is the error message for Ajax
 
-    // Create the PlaceService and send the request.
-    // Handle the callback with an anonymous function.
-  // var service = new google.maps.places.PlacesService(map);
-  // service.nearbySearch({
-  //   location: sanFrancisco,
-  //   radius: 500,
-  //   types: ['stores'],
-  // }, callback); 
+    var errorAjax = "Whoops. Better luck finding your date an Uber. Can't find any data";
 
-    };
-  function callback(results, status){
-    if(status === google.maps.places.PlacesServiceStatus.OK){
-      for(var i=0; i<results.length; i++){
-        createMarker(results[i]);
+    var infoWindow = new google.maps.InfoWindow({
+      content: errorAjax
+    });
+
+    // IIFE to solve closure issue 
+    marker.addListener('click', (function(pinCopy, infoWindowCopy){
+        return function(){
+          closeInfoWindows();
+          infoWindowCopy.open(map, pinCopy);
+          toggleBounceOffAll();
+          toggleBounceOn(pinCopy);
+        };
+      })(marker, infoWindow));
+
+    // push each marker into marker's array to make them observable
+    pins.push(marker);
+
+    // push infoWindow to the infoWindow's array to make them observable
+    infoWindows.push(infoWindow);
+  }
+}
+
+var toggleOff = function(marker) {
+    marker.setMap(null);
+};
+var toggleOn = function(marker) {
+    marker.setMap(map);
+};
+var toggleOffAll = function() {
+    for (var x in pins) {
+        pins[x].setMap(null);
+    }
+};
+
+// function to close all info windows
+var closeInfoWindows = function() {
+    for (var x in infoWindows) {
+        infoWindows[x].close();
+    }
+};
+
+// functions to toggle pin's BOUNCE animation
+var toggleBounceOffAll = function() {
+    for (var x in pins) {
+        pins[x].setAnimation(null);
+    }
+};
+
+var toggleBounceOn = function(marker) {
+    marker.setAnimation(google.maps.Animation.BOUNCE);
+};
+
+/** VIEWMODEL **/
+
+var viewModel = {
+  // data
+  pins: ko.observableArray(locations2),
+  searchValue: ko.observable(''),
+
+  // ops
+  search: function(value){
+    viewModel.pins.removeAll();
+    toggleOffAll();
+
+    for (var x in locations){
+      if(locations[x].name.toLowerCase().indexOf(value.toLowerCase()) >= 0 ){
+      viewModel.pins.push(locations[x]);
+      toggleOn(pins[x]);
+      }
+    }
+  },
+  listValue: function(value){
+    // closes all info windows, toggles off all Bounce
+    closeInfoWindows();
+    toggleBounceOffAll();
+
+    for(var x in locations){
+      if(locations[x].name.toLowerCase().indexOf(value.toLowerCase()) >= 0 ){
+        // open the clicked marker's infoWindow and trigger animation
+        infoWindows[x].open(map, pins[x]);
+        toggleBounceOn(pins[x]);
       }
     }
   }
-  // function createMarker(place){
-  //   var placeLoc = place.geometry.location;
-  //   var marker = new google.maps.Marker({
-  //     map : Map,
-  //     position : place.geometry.location
-  //   });
-      // Run the initialize function when the window has finished loading.
-  // google.maps.event.addListener(marker, 'click', function(){
-  //   infowindow.setContent(place.name);
-  //   infowindow.open(map, this);
-  //   });
-  // };
-
-  var flickr;
+};
+ko.applyBindings(viewModel);
+viewModel.searchValue.subscribe(viewModel.search);
   
+
+var googleError = function() {
+    alert("Sorry, Google Maps API can't be loaded now. Please try later.");
+    alertCount = false;
+};  

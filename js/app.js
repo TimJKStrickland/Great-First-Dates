86d0c2778bf03ce93ec524159f94287a8ba8be28
Tim JK Strickland <timjkstrickland@gmail.com>
2016-02-07 15:30:57 -0800

    Fix app.js to make description its own line

--- js/app.js
+++ js/app.js
@@ -287,10 +287,10 @@ for( var x in locations){
                 contentString1 += "<span>This place is so hip they don't even have a phone.</span>";
               }  
             for (var i=0; i < categories.length; i++) {
-                contentString1 += '<span>' + categories[i].name + '</span>';
+                contentString1 += '<p>' + categories[i].name + ' </p>';
               }
-              // delete last two positions of contentString1. Only category wanted per hit
-              contentString2 = contentString2.slice(0, -2);
+              // delete last two positions of contentString2. Only category wanted per hit
+              contentString2 = contentString2.slice(0, -1);
               var contentString = contentString0 + contentString1 + contentString2 + contentString3;
 
               // change info windows' content

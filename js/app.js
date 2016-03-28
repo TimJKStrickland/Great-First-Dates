372ba2b00349e6f6f3e25a3f1c5e322c6c2241be
Tim JK Strickland <timjkstrickland@gmail.com>
2016-03-28 12:16:42 -0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -2,12 +2,7 @@ var map;
 var startCenter = {lat: 37.7764823, lng: -122.42};
 contentString = "";
 var errorAjax = "Whoops. Better luck finding your date an Uber. Can't find any data.";
-$(document).keyup(function(e){
-  if(e.keyCode == 27){ 
-    google.maps.event.trigger(e.location.marker, 'closeclick');
-    console.log('escape pushed');
-  }
-});
+
 /** VIEWMODEL **/
 var ViewModel = function(){
   var self = this;
